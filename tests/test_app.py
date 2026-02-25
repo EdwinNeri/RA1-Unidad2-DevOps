5ad532e8b6297a82aaf7d7d8e0ee25dcf168f151
EdwinNeri <115720574+EdwinNeri@users.noreply.github.com>
2026-02-24 22:07:23 -0600

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app.app import suma
+
+class TestApp(unittest.TestCase):
+
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
